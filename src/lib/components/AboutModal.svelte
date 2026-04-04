ea2280f84847e1bcf8414211e920f687b1ced3ba
vicrodh <103399283+vicrodh@users.noreply.github.com>
2026-04-04 11:19:52 -0600

    chore: update codename to Bye bye plain auth
    cc

--- src/lib/components/AboutModal.svelte
+++ src/lib/components/AboutModal.svelte
@@ -100,7 +100,7 @@
             <span class="label">{$t('about.buildInfo.version')}</span>
             <span class="value">{appVersion}</span>
             <span class="label">{$t('about.buildInfo.codename')}</span>
-            <span class="value codename">Limpiando la casa</span>
+            <span class="value codename">Bye bye plain auth</span>
             <span class="label">{$t('about.buildInfo.license')}</span>
             <span class="value">MIT</span>
             <span class="label">{$t('about.buildInfo.license')}</span>
